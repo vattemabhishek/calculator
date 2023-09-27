bfa7363942f1b0b9bb426a28a82926630cf7ad8a
vattemabhishek <vattemabhishek@gmail.com>
2023-09-27 14:57:59 +0530

    third commit

---
+++ calci.js
@@ -0,0 +1,49 @@
+function functionToExec(a) {
+    let curVal = "";
+    let storedVal = "";
+    let operator='';
+    
+    switch (a) {
+         case (add):
+        {
+
+        } break;
+        case (subtract):
+        {
+
+        }break;
+        case(equal):
+        {
+
+        }break;
+        case(remainder) :
+        {
+
+        }break;
+        case(div):
+        {
+
+        }break;
+        case(product):
+        {
+
+        }break;
+        case(plus/minus):
+        {
+
+        }break;
+        case(decimal):
+        {
+
+        }break;
+        default :
+        {
+                if(curVal && storedVal==="") 
+                {
+                   curval = curval+ a.toString();         
+                }
+        }break;
+    }
+    
+    
+};
