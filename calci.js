4913187087c4f7026bd4b4ac8925dbaa9b805600
Indra Teja <indratejamerugu@gmail.com>
2023-09-28 18:47:43 +0530

    fifth

--- calci.js
+++ calci.js
@@ -1,55 +1,88 @@
+let curVal = '';
+let storedVal = '';
+let operator = '';
+
 function functionToExec(a) {
-    let curVal = "";
-    let storedVal = "";
-    let operator='';
-    
-    switch (a) {
-         case (add):
-        {
-                operator = 'add';
-        } break;
-        case (subtract):
-        {
-                operator = 'subtract';
-        }break;
-        case(equal):
-        {
 
-        }break;
-        case(remainder) :
-        {
-                operator = 'remainder';
-        }break;
-        case(div):
-        {
-                operator = 'div';
-        }break;
-        case(product):
-        {
-                operator = 'product';
-        }break;
-        case(plus/minus):
-        {
+  switch (a) {
+    case 'cancel':
+      {
+        curVal = '';
+        storedVal = '';
+        operator = '';
+      } break;
+    //if operator is not empty then repeat equals for second time oprrator entry
+    case ('add'):
+    case ('subtract'):
+    case ('remainder'):
+    case ('div'):
+    case ('product'):
+      {
+        if (operator !== '') {
+          equal();
+        }
 
-        }break;
-        case(decimal):
-        {
-            
-        }break;
-        default :
-        {
-                if(curVal && storedVal==="") 
-                {
-                   curval = curval + a.toString();         
-                }
-                else
-                {
-                    curval = a.toString();
-                }
-        }break;
-    }
+        operator = a;
+        storedVal = curVal;
 
-    
-    
-    
+      } break;
+    case ('equal'):
+      {
+        equal();
+      } break;
+    case ('plus/minus'):
+      {
+        curVal *= -1;
+      } break;
+    case ('decimal'):
+      {
+        curVal += '.';
+      } break;
+    default:
+      {
+        //curvalue and op are empty --> cur value assigned to arg
+        //curvalue exits and op is empty --> arg appends to curvalue
+        //curvalue  and op exists--> cur value is stored and then cur value is assigned with new arg
+        //curvalue is not empty and operator exists -> append to current value
+        //
+        // if(operator) {
+
+        // }
+        console.log("🚀 ~ file: calci.js:53 ~ functionToExec ~ curVal:", curVal, operator, storedVal, a)
+
+        if (curVal !== '' && curVal !== storedVal) {
+          curVal += a.toString();
+        }
+        else {
+          curVal = a.toString();
+        }
+      } break;
+
+  }
+  console.log(curVal);
+  const displayBar = document.getElementById('display');
+  displayBar.innerHTML = curVal;
+
 };
+
+function equal() {
+  if (operator === 'add') {
+    curVal = parseFloat(storedVal) + parseFloat(curVal);
+  }
+  else if (operator === 'subtract') {
+    curVal = parseFloat(storedVal) - parseFloat(curVal);
+  }
+  else if (operator === 'product') {
+    curVal *= storedVal;
+    parseFloat(curVal).toFixed(8);
+  }
+  else if (operator === 'remainder') {
+    curVal %= storedVal;
+    parseFloat(curVal).toFixed(5);
+  }
+  else if (operator === 'div') {
+    curVal /= storedVal;
+    parseFloat(curVal).toFixed(5);
+  }
+  operator = '';
+}
